fa7fd3e06e61fa2074e52e1606b36358c62c4df7
Erick Merchant <erickmerchant@gmail.com>
2015-04-15 23:37:46 -0700

    Add test coverage

--- test.js
+++ test.js
@@ -6,10 +6,7 @@ var first = require('./index.js')
 describe('plugin', function () {
   it('it should return the first page', function (done) {
     first([{title: 'one'}, {title: 'two'}, {title: 'three'}], function (err, pages) {
-      if (err) {
-        throw err
-      }
-
+      assert.equal(null, err)
       assert.deepEqual(pages, [{title: 'one'}])
 
       done()
@@ -18,10 +15,7 @@ describe('plugin', function () {
 
   it('it should return an empty array if pages is empty', function (done) {
     first([], function (err, pages) {
-      if (err) {
-        throw err
-      }
-
+      assert.equal(null, err)
       assert.deepEqual(pages, [])
 
       done()
